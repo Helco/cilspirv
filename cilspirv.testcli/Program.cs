ec9408bb294f2ac43bdb98e78731a4880ea103c7
Helco <hermann.noll@hotmail.com>
2021-08-10 19:45:55 +0200

    Basic project structure

--- cilspirv.testcli/Program.cs
+++ cilspirv.testcli/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
 namespace cilspirv.testcli
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assembly = typeof(test_shaders.Simple).Assembly;
+            var assemblyDef = Mono.Cecil.AssemblyDefinition.ReadAssembly(assembly.Location);
+            Console.WriteLine(assemblyDef.MainModule.Types[1].FullName);
         }
     }
 }
---
+++ test-shaders/Simple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+namespace test_shaders
+{
+    public class Simple
+    {
+        public Vector4 Frag()
+        {
+            return new Vector4(0.1f, 0.3f, 0.7f, 1f);
+        }
+    }
+}
