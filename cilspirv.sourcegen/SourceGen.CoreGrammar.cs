a1148d1e115d11c722b21155aee6b00d5105a1a0
Helco <hermann.noll@hotmail.com>
2021-08-12 06:20:17 +0200

    Start of spirv generator

---
+++ cilspirv.sourcegen/SourceGen.CoreGrammar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using cilspirv.SourceGen.CoreGrammar;
+
+namespace cilspirv.SourceGen.CoreGrammar
+{
+    public class Rootobject
+    {
+        public string[] copyright { get; set; }
+        public string magic_number { get; set; }
+        public int major_version { get; set; }
+        public int minor_version { get; set; }
+        public int revision { get; set; }
+        public Instruction_Printing_Class[] instruction_printing_class { get; set; }
+        public Instruction[] instructions { get; set; }
+        public Operand_Kinds[] operand_kinds { get; set; }
+    }
+
+    public class Instruction_Printing_Class
+    {
+        public string tag { get; set; }
+        public string heading { get; set; }
+    }
+
+    public class Instruction
+    {
+        public string opname { get; set; }
+        public string @class { get; set; }
+        public int opcode { get; set; }
+        public Operand[] operands { get; set; }
+        public string[] capabilities { get; set; }
+        public string lastVersion { get; set; }
+        public string version { get; set; }
+        public string[] extensions { get; set; }
+    }
+
+    public class Operand
+    {
+        public string kind { get; set; }
+        public string name { get; set; }
+        public string quantifier { get; set; }
+    }
+
+    public class Operand_Kinds
+    {
+        public string category { get; set; }
+        public string kind { get; set; }
+        public Enumerant[] enumerants { get; set; }
+        public string doc { get; set; }
+        public string[] bases { get; set; }
+    }
+
+    public class Enumerant
+    {
+        public string enumerant { get; set; }
+        public object value { get; set; }
+        public string[] capabilities { get; set; }
+        public Parameter[] parameters { get; set; }
+        public string version { get; set; }
+        public string[] extensions { get; set; }
+        public string lastVersion { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string kind { get; set; }
+        public string name { get; set; }
+        public string quantifier { get; set; }
+    }
+
+}
---
+++ cilspirv.sourcegen/SourceGen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Newtonsoft.Json;
+
+namespace cilspirv.SourceGen
+{
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var generateSpirvEnums =
+                context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.GenerateSpirvEnums", out var generateSpirvEnumSwitch) &&
+                generateSpirvEnumSwitch.Equals("true", StringComparison.OrdinalIgnoreCase);
+
+            var generateSpirvInstructions =
+                context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.GenerateSpirvInstructions", out var generateSpirvInstructionsSwitch) &&
+                generateSpirvInstructionsSwitch.Equals("true", StringComparison.OrdinalIgnoreCase);
+
+            var generateSpirvExtInstructions =
+                context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.GenerateSpirvExtInstructions", out var generateSpirvExtInstructionsSwitch) &&
+                generateSpirvExtInstructionsSwitch.Equals("true", StringComparison.OrdinalIgnoreCase);
+
+            var assembly = typeof(SourceGenerator).Assembly;
+            var coreGrammarStream = assembly.GetManifestResourceStream("spirv.core.grammar.json");
+            var coreGrammarReader = new StreamReader(coreGrammarStream);
+            using var coreGrammarJsonReader = new JsonTextReader(coreGrammarReader);
+            var coreGrammar = new JsonSerializer().Deserialize<CoreGrammar.Rootobject>(coreGrammarJsonReader);
+        }
+
+        public void Initialize(GeneratorInitializationContext context) { }
+    }
+}
