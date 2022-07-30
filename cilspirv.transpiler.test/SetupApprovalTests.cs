6e5a60d3f2a144f3a85f7ff88ca677e08b7eb52c
Helco <hermann.noll@hotmail.com>
2022-07-30 19:49:35 +0200

    Run spirv-val on approved test modules

--- cilspirv.transpiler.test/SetupApprovalTests.cs
+++ cilspirv.transpiler.test/SetupApprovalTests.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using ApprovalTests.Reporters;
 using DiffEngine;
+using NUnit.Framework;
 
 [assembly: UseReporter(typeof(DiffReporter))]
 
 namespace cilspirv.transpiler.test
 {
-    internal static class SetupApprovalTests
+    internal class SetupApprovalTests
     {
         [ModuleInitializer]
         internal static void Setup()
         {
             DiffTools.UseOrder(DiffTool.VisualStudio);
+        }
+
+        [Test]
+        public void IsSpirvValPresent()
+        {
+            var process = new Process()
+            {
+                StartInfo =
+                {
+                    FileName = "spirv-val",
+                    Arguments = "--version",
+                    RedirectStandardOutput = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            var output = process.StandardOutput.ReadToEnd();
+            Assert.AreEqual(0, process.ExitCode);
+            Assert.True(output.Contains("SPIR-V 1.0"));
         }
     }
 }
